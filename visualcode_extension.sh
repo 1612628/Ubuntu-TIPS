f6d0a03acfa7ea8044ec8ef1773e71311f29cbb5
1612628 <thanhnguyenduy2304@gmail.com>
2019-03-30 12:33:14 +0700

    add web development extensions

---
+++ visualcode_extension.sh
@@ -0,0 +1,24 @@
+#Web Development
+#----------------------------
+#HTML Extensions
+code --install-extension formulahendry.auto-close-tag;
+code --install-extension formulahendry.auto-rename-tag;
+code --install-extension abusaidm.html-snippets;
+#CSS
+code --install-extension thekalinga.bootstrap4-vscode;
+code --install-extension pranaygp.vscode-css-peek;
+code --install-extension zignd.html-css-class-completion;
+#Javascript Extensions
+code --install-extension msjsdiag.debugger-for-chrome;
+code --install-extension xabikos.javascriptsnippets;
+code --install-extension donjayamanne.jquerysnippets;
+#Node.js
+code --install-extension leizongmin.node-module-intellisense;
+code --install-extension christian-kohler.path-intellisense;
+code --install-extension formulahendry.terminal;
+code --install-extension andrejunges.handlebars;
+#General Exntensions
+code --install-extension remimarsal.prettier-now;
+code --install-extension anseki.vscode-color;
+code --install-extension coenraads.bracket-pair-colorizer;
+code --install-extension qinjia.view-in-browser;
